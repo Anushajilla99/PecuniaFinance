42bcc9f180e0c32beb985dac7436609802304e3d
AnushaJilla <16h61a1276@cvsr.ac.in>
2020-04-20 15:48:44 +0530

    first commit

---
+++ PecuniaFinanceSystem1/src/main/java/com/example/demo/PecuniaFinanceSystem1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecuniaFinanceSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PecuniaFinanceSystem1Application.class, args);
+	}
+
+}
