a1dc4a69b52eb21882971bf229bbdf1ef1ec7420
Archana pabbathi <Archana pabbathi@LAPTOP-LVI8VF29>
2020-04-29 20:50:36 +0530

    Archana29

--- PecuniaFinanceSystem1/src/main/java/com/capgemini/pecunia/controller/AccountController.java
+++ PecuniaFinanceSystem1/src/main/java/com/capgemini/pecunia/controller/AccountController.java
@@ -27,7 +27,6 @@ import com.capgemini.pecunia.bean.Account;
 public class AccountController {
 	@Autowired
 	AccountService serviceobj;
-
 	// Create Account
 	@PostMapping("/AccountCreation")
 	public ResponseEntity<String> accountCreation(@RequestBody Account ac) {
